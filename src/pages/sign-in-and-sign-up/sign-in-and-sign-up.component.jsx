0a46809d21f6d95fabe7dd696168dd6fbf554608
Samir <42714951+sameerrM@users.noreply.github.com>
2019-10-28 13:25:26 +0100

    adding sign in component and custom button

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SigIn from '../../components/sign-in/sign-in.component'
+
+import './sign-in-and-sign-up.styles.scss'
+
+const SignInSignUp = () => {
+    return (
+        <div className="sing-in-and-sign-up">
+            <SigIn />
+        </div>
+    )
+}
+
+export default SignInSignUp;
